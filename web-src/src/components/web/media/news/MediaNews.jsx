eed204089915ae1ec26ab16059c532cdf2012a6a
DESKTOP-Q1UBL2N\nyoon <ye.jeong@hicomp.net>
2024-06-03 17:58:17 +0900

    뉴스 게시판 퍼블

--- web-src/src/components/web/media/news/MediaNews.jsx
+++ web-src/src/components/web/media/news/MediaNews.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import Header from "components/web/common/header";
 
 const MediaNews = () => {
     return (
-        <div>
-            <div>뉴스</div>
-        </div>
+        <>
+        <Header />
+        <div id="subvisual">
+                <div className="sub_txt">
+                    <h2>MEDIA CENTER</h2>
+                </div>
+                <div id="leftmenu">
+                    <a href="#sec01" className="active">NEWS</a>
+                    <a href="#sec02">공지사항</a>
+                </div>
+            </div>
+            <div id="con_area">
+                <div className="medicenter">
+                        <h3 className="c_tit">
+                            <span>메디씨티의 소식을 전합니다</span>
+                            NEWS
+                        </h3>
+                    <div className="boxwrap" >
+                        <div className="box">
+                            <a href="">
+                                <span><img src="img/web/sub/news_test.jpg" alt="테스트"></img></span>
+                                <p className="name">제1회 메디씨티 쇼케이스 (인니) 현장스케치</p>
+                                <a href="" className="btn_main">VIEW MORE</a>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </>
     );
 };
 
