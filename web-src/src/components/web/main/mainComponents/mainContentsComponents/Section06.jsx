a4eff3f0c82afe22f3c559fcd9f589090223765d
용광순 <gs.yong@hicomp.net>
2024-06-27 15:33:04 +0900

    SEO 작업중

--- web-src/src/components/web/main/mainComponents/mainContentsComponents/Section06.jsx
+++ web-src/src/components/web/main/mainComponents/mainContentsComponents/Section06.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Arrow from "components/web/common/Arrow";
 import routerPath from "etc/lib/path/routerPath";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import LineBreak from "etc/lib/language/web/LineBreak";
 
 const Section06 = () => {
@@ -21,7 +21,10 @@ const Section06 = () => {
                             {/*메디씨티는 한국의 의료기술을 세계에 알림으로써<br />*/}
                             {/*전세계의 많은 환자들이 보다 좋은 의료서비스를 통해<br />*/}
                             {/*건강해지기를 진심으로 기원합니다.*/}
-                            {LineBreak(t("main.sec06.content"))}
+                            <Trans
+                                i18nKey={"main.sec06.content"}
+                                components={[<br></br>]}
+                            />
                         </p>
                     </div>
                 </div>
