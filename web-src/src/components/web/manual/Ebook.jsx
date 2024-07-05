92db806c4018ead801ae19a691ee26c4ab247ff4
용광순 <gs.yong@hicomp.net>
2024-07-05 10:40:29 +0900

    이북 테스트

--- web-src/src/components/web/manual/Ebook.jsx
+++ web-src/src/components/web/manual/Ebook.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Ebook = () => {
-    return <div></div>;
+const Ebook = (props) => {
+    /**
+     * ebook 히스토리 여기에 지정
+     * 240705 - test : /ebook/symposium
+     * @returns {{__html: string}}
+     */
+    const ebookIndex = () => {
+        return {
+            __html: '<iframe src="/ebook/symposium/index.html" width="100%" height="100%"></iframe>',
+        };
+    };
+
+    return (
+        <>
+            <div className="wrapper">
+                <div
+                    dangerouslySetInnerHTML={ebookIndex()}
+                    style={{ height: "100vh" }}
+                ></div>
+            </div>
+        </>
+    );
 };
 
 export default Ebook;
